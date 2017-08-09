ed363fbd4687411f8232426a3636d02c38acad3d
young na <ygna91@gmail.com>
2017-08-09 10:54:54 +0900

    CORS 문제 해결

--- transactionApi/server.js
+++ transactionApi/server.js
@@ -3,7 +3,8 @@ let express = require('express'),
   port = process.env.PORT || 3000,
   mongoose = require('mongoose'),
   Models = require('./api/models/mkbModel'),
-  bodyParser = require('body-parser');
+  bodyParser = require('body-parser'),
+  cors = require('cors');
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/mkb', {useMongoClient:true,});
@@ -11,6 +12,14 @@ mongoose.connect('mongodb://localhost/mkb', {useMongoClient:true,});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+ res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+    next();
+});
+
+출처: http://m.mkexdev.net/339 [박종명의 아름다운 개발 since 2010.06]
 
 
 var routes = require('./api/routes/mkbRoutes');
