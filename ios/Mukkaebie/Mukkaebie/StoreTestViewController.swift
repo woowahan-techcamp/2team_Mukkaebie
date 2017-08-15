bdc986532f6d78a0241bce548e6326bfc1475959
danb11 <miliam5234@naver.com>
2017-08-15 20:40:56 +0900

    storedetail 테이블뷰로 리팩토링

---
+++ ios/Mukkaebie/Mukkaebie/StoreTestViewController.swift
@@ -0,0 +1,103 @@
+//
+//  StoreTestViewController.swift
+//  Mukkaebie
+//
+//  Created by woowabrothers on 2017. 8. 15..
+//  Copyright © 2017년 woowabrothers. All rights reserved.
+//
+
+import UIKit
+
+//struct postStruct {
+//    var image : UIImage!
+//    var name : String!
+//}
+
+
+class StoreTestViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var rateView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        if section == 0 {
+            let headerView = Bundle.main.loadNibNamed("statusTableViewCell", owner: self, options: nil)?.first as! statusTableViewCell
+            return headerView
+        }
+        
+        if section == 1 {
+            let logoView = Bundle.main.loadNibNamed("logoTableViewCell", owner: self, options: nil)?.first as! logoTableViewCell
+            logoView.logoImage.image = #imageLiteral(resourceName: "woowatech")
+            return logoView
+            
+        }
+            
+        else if section == 2 {
+            let rateView = Bundle.main.loadNibNamed("rateTableViewCell", owner: self, options: nil)?.first as! rateTableViewCell
+            return rateView
+        }
+        return self.view
+    }
+    
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        if section == 0 {
+            return 36
+        }
+        else if section == 1 {
+            return 155
+        }
+        else if section == 2 {
+            return 96
+        }
+        return 0
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return 0
+        }
+        else if section == 1 {
+            return 0
+        }
+        else if section == 2 {
+            return 0
+        }
+        
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        cell?.textLabel?.text = "\(indexPath.row)"
+        return cell!
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ios/Mukkaebie/Mukkaebie/statusTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  statusTableViewCell.swift
+//  Mukkaebie
+//
+//  Created by woowabrothers on 2017. 8. 15..
+//  Copyright © 2017년 woowabrothers. All rights reserved.
+//
+
+import UIKit
+
+class statusTableViewCell: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
