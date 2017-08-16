5280d95e64d5e885749ff9f98ed6725376d38df5
Hyeongjong Kim <fa68420@gmail.com>
2017-08-16 14:07:45 +0900

    주석 지움

--- ios/Mukkaebie/Mukkaebie/NetworkOrder.swift
+++ ios/Mukkaebie/Mukkaebie/NetworkOrder.swift
@@ -35,20 +35,3 @@ class NetworkOrder {
     
     
 }
-
-
-//func getStoreList() {
-//    Alamofire.request("\(url)stores").responseJSON { (response) in
-//        if let response = response.result.value as? [[String:Any]] {
-//            var storeList = [ModelStores]()
-//            for item in response {
-//                var store = ModelStores(JSON: item)
-//                storeList.append(store!)
-//                
-//            }
-//            
-//            NotificationCenter.default.post(name: NSNotification.Name(rawValue: "getStore"), object: nil, userInfo: ["storeList":storeList])
-//        }
-//    }
-//}
-//    
