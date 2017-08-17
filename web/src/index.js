b89e0f3b296a12033d7125c8be84bdaac3acec11
heeinso <heeinso@snu.ac.kr>
2017-08-17 16:37:28 +0900

    인덱스 페이지에 스크립트로 있던 JS 코드들 store.js로 분리시키고 리팩토링

--- web/src/index.js
+++ web/src/index.js
@@ -6,6 +6,8 @@ import {
   makeOrder,
   toggleMobileCategory,
   Graph,
+  StoreList,
+  StoreInfo,
   StoreUtil
 } from './store.js'
 
@@ -13,39 +15,41 @@ import {
 import style from './scss/main.scss';
 
 
-// document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () {
+
+  console.log("loaded!");
 
 
-  let review = new Review();
+  // let review = new Review();
+  //
+  //
+  // let tab = new TabUiWithAjax(
+  //     {
+  //       containerName: "storeTabWrapper",
+  //       selectedTabName: "selectedTab",
+  //       selectedContentName: "selectedContent",
+  //       generalTabName: "storeTab",
+  //       generalContentPrefix: "#cont-",
+  //       baseUrl: "",
+  //     }
+  // );
 
-
-  let tab = new TabUiWithAjax(
-      {
-        containerName: "storeTabWrapper",
-        selectedTabName: "selectedTab",
-        selectedContentName: "selectedContent",
-        generalTabName: "storeTab",
-        generalContentPrefix: "#cont-",
-        baseUrl: "",
-      }
-  );
-
-  let foldable = new Foldable("foldableLevel1");
-
-  let graph = new Graph();
+  // let foldable = new Foldable("foldableLevel1");
+  //
+  // let graph = new Graph();
 
   // StoreUtil.scrollWithCart();
   //
   // StoreUtil.makeOrder.call(graph);
 
-  document.querySelector('#mkbTab').addEventListener('click', function () {
-    let graph = new Graph();
-    graph.podiumAnimate();
-  });
-
-  document.querySelector(".mobileTitleButton").addEventListener("click", function () {
-    StoreUtil.toggleMobileCategory();
-  })
+  // document.querySelector('#mkbTab').addEventListener('click', function () {
+  //   let graph = new Graph();
+  //   graph.podiumAnimate();
+  // });
+  //
+  // document.querySelector(".mobileTitleButton").addEventListener("click", function () {
+  //   StoreUtil.toggleMobileCategory();
+  // })
 
   let clickedStore = document.querySelector("#storeListTemplate");
 
@@ -56,5 +60,51 @@ import style from './scss/main.scss';
     let storeInfo = new StoreInfo(e.target.parentNode.value);
   });
 
+
+  let clickedMenu = document.querySelector(".categoryWrapper");
+
+  console.log(clickedMenu);
+
+  clickedMenu.addEventListener("click", function (e) {
+    if (e.target.tagName !== "LI") {
+      console.log(e.target);
+      e.target = e.target.closest(".blackMenu");
+      console.log(e.target);
+    }
+    let categoryList = new StoreList(e.target.attributes.data.value);
+  });
+
+
+  let clickedMobileMenu = document.querySelector(".mobileCategory");
+
+  clickedMobileMenu.addEventListener("click", function (e) {
+    if (e.target.className !== "col-xs-4 mobileMenu") {
+      e.target = e.target.closest(".mobilekMenu");
+    }
+    let categoryList = new StoreList(e.target.attributes.data.value);
+  });
+
+  let layoutTarget = document.querySelector('.storeLayout');
+
+  layoutTarget.addEventListener("click", function () {
+
+    if (layoutTarget.children.item(2)) {
+
+      let tab = new TabUiWithAjax(
+          {
+            containerName: "storeTabWrapper",
+            selectedTabName: "selectedTab",
+            selectedContentName: "selectedContent",
+            generalTabName: "storeTab",
+            generalContentPrefix: "#cont-",
+            baseUrl: "",
+          }
+      );
+
+      let foldable = new Foldable("foldableLevel1");
+    }
+  });
+
+});
 
 
