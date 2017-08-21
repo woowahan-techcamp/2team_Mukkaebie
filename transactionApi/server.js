76e0e1f344af126305b6a6420f46a8cc8ea75020
young na <ygna91@gmail.com>
2017-08-22 00:32:53 +0900

    이미지 데이터 업로드 구현

--- transactionApi/server.js
+++ transactionApi/server.js
@@ -29,5 +29,48 @@ routes(app);
 
 app.listen(port);
 
+//
+
+
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+
+var index = require('./routes/index');
+var profile = require('./routes/profile');
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+app.use('/', index);
+app.use('/profile', profile);
+
+
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+
+app.use(function(err, req, res, next) {
+
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+});
+//
+
 
 console.log('Mukkaebie RESTful API server started on: ' + port);
