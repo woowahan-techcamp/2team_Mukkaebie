18318fc243b96e512868ffc20daecdc84be83376
younguna <ygna91@gmail.com>
2017-08-22 11:19:05 +0900

    body parser Urlencoded 옵션 변경

--- transactionApi/server.js
+++ transactionApi/server.js
@@ -45,8 +45,6 @@ app.set('view engine', 'jade');
 
 
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
