6efd5b302494e071111ff5032480d1ef34ba1c70
heeinso <heeinso@snu.ac.kr>
2017-08-23 19:31:16 +0900

    충돌 해결하며 누락된 코드 되돌림

--- web/src/store.js
+++ web/src/store.js
@@ -184,7 +184,7 @@ class MKBComment {
     this.postImage().then(this.postComment.bind(null, id)).then(this.getComment.bind(null, id, top3List));
   }
 
-  makeMKBModal (id, top3List) {
+  makeMKBModal(id, top3List) {
 
     let modal = document.querySelector('#mkbModal');
     let modalBtn = Array.from(document.querySelectorAll(".mkbProfileImgs"));
@@ -195,7 +195,7 @@ class MKBComment {
       circle.addEventListener("click", function (event) {
 
         function getResponse() {
-          return new Promise(function(resolve){
+          return new Promise(function (resolve) {
             let xhr = new XMLHttpRequest();
             xhr.onreadystatechange = function () {
               if (this.readyState == 4 && this.status == 200) {
@@ -203,7 +203,7 @@ class MKBComment {
                 resolve(response)
               }
             };
-            xhr.open("GET", SERVER_BASE_URL + "/stores/" + id , true);
+            xhr.open("GET", SERVER_BASE_URL + "/stores/" + id, true);
             xhr.send();
           })
         }
@@ -214,26 +214,26 @@ class MKBComment {
           previewTarget.style.backgroundImage = event.target.style.backgroundImage;
           const mkbResponse = res[0]["mkb"];
           let clickedUser = "";
-          if (this.attributes["data-user"] != undefined || this.attributes["data-user"] != null){
+          if (this.attributes["data-user"] != undefined || this.attributes["data-user"] != null) {
             let clickedUser = this.attributes["data-user"]["value"];
             let clickedMkb;
 
-            let clickeMkbData = mkbResponse.filter(function(mkb){
+            let clickeMkbData = mkbResponse.filter(function (mkb) {
               return mkb["userId"] == clickedUser;
             });
-            if ( clickeMkbData.length > 0){
-              clickedMkb = clickeMkbData[clickeMkbData.length-1];
+            if (clickeMkbData.length > 0) {
+              clickedMkb = clickeMkbData[clickeMkbData.length - 1];
             }
 
-            if (clickedMkb){
+            if (clickedMkb) {
               document.getElementById("commentTextInput")["value"] = clickedMkb["mkbComment"];
             }
-            else{
+            else {
               document.getElementById("commentTextInput")["value"] = "";
             }
             const commentUser = document.querySelector(".commentWriteBox p");
             commentUser.innerText = clickedUser;
-            commentUser.setAttribute("value",this.attributes["value"]["value"]);
+            commentUser.setAttribute("value", this.attributes["value"]["value"]);
           }
         }
         getResponse().then(renderModal);
@@ -244,7 +244,7 @@ class MKBComment {
       });
     })
 
-    closeBtn.addEventListener("click", function(){
+    closeBtn.addEventListener("click", function () {
       const editBox = document.querySelector(".logInRequired");
       const editBtn = document.querySelector(".mkbEdit");
       modal.style.opacity = "0";
@@ -267,21 +267,21 @@ class MKBComment {
         let finalMkbList = [];
         top3List.forEach(function (topBuyer) {
 
-          let oneBuyer = targetArr.filter(function(mkbRow){
+          let oneBuyer = targetArr.filter(function (mkbRow) {
 
             return mkbRow.userId == topBuyer;
           });
 
 
-          if ( oneBuyer.length > 0){
-            finalMkbList.push(oneBuyer[oneBuyer.length-1]);
+          if (oneBuyer.length > 0) {
+            finalMkbList.push(oneBuyer[oneBuyer.length - 1]);
           } else {
             finalMkbList.push(topBuyer);
           }
 
         });
 
-        let mkbLevelList = [ "gold","silver","bronze" ];
+        let mkbLevelList = ["gold", "silver", "bronze"];
         finalMkbList.forEach(function (topBuyer, idx) {
           renderContent(topBuyer, mkbLevelList[idx]);
         });
@@ -290,7 +290,7 @@ class MKBComment {
         function renderContent(oneComment, mkbLevel) {
           const comment = oneComment;
           const profilePicSmall = document.querySelector("." + mkbLevel + "Img");
-          if (typeof(comment) === "string"){
+          if (typeof(comment) === "string") {
             profilePicSmall.setAttribute("data-user", comment);
             profilePicSmall.style.backgroundImage = "url('" + DEFAULT_PROFILE_IMG + "')";
           } else {
@@ -308,12 +308,12 @@ class MKBComment {
 
 
   postImage(id) {
-    return new Promise(function(resolve){
+    return new Promise(function (resolve) {
       const form = document.getElementById('file-form');
       const fileSelect = document.getElementById('file-select');
       const uploadButton = document.getElementById('upload-button');
 
-      form.onsubmit = function(event) {
+      form.onsubmit = function (event) {
         event.preventDefault();
         uploadButton.innerHTML = '바꾸는 중...';
         const file = fileSelect.files[0];
@@ -330,7 +330,7 @@ class MKBComment {
               resolve(IMAGE_SERVER_URL + "/uploads/" + res["filename"])
               const profilePic = document.querySelector(".mkbImgPreview");
               const targetCircle = document.querySelector(".commentWriteBox p")
-              const profilePicSmall = document.querySelector("."+ targetCircle.getAttribute("value") +"Img");
+              const profilePicSmall = document.querySelector("." + targetCircle.getAttribute("value") + "Img");
               profilePic.style.backgroundImage = "url('" + IMAGE_SERVER_URL + "/uploads/" + res["filename"] + "')";
               profilePicSmall.style.backgroundImage = "url('" + IMAGE_SERVER_URL + "/uploads/" + res["filename"] + "')";
             }
@@ -354,7 +354,7 @@ class MKBComment {
 
   postComment(id, imgUrl) {
 
-    return new Promise(function(resolve){
+    return new Promise(function (resolve) {
 
       let packet = {"mkb": {}};
       packet["storeId"] = id;
@@ -375,7 +375,9 @@ class MKBComment {
         document.querySelector("#commentTextInput").setAttribute("value", "");
       }.bind(this)
 
-})}}
+    })
+  }
+}
 
 
 class Graph {
@@ -632,7 +634,7 @@ class StoreList {
           const storeImg = store.storeImg;
           const storeName = store.storeName;
           const address = store.address;
-          if(store.review) {
+          if (store.review) {
             const reviewCount = store.review.length;
           }
           const ratingCount = store.ratingCount;
@@ -937,9 +939,9 @@ let StoreUtil = {
       }, 500)
       setTimeout(function () {
         modal.style.opacity = 0;
-        setTimeout(function(){
+        setTimeout(function () {
           modal.style.display = "none";
-        },500)
+        }, 500)
       }, 3000)
 
     })
@@ -1004,10 +1006,10 @@ let StoreUtil = {
     let x = document.querySelector('.mobileCategory');
     let btn = document.querySelector(".mobileTitleButton");
     if (btn.classList.contains("unfolded")) {
-      x.style.display = 'none';
+      x.classList.remove("mobileCategoryShow");
       btn.classList.remove("unfolded");
     } else {
-      x.style.display = 'block';
+      x.classList.add("mobileCategoryShow");
       btn.classList.add("unfolded");
     }
   }
