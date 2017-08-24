974ab298f908ef71760615bbfac0c4de61acbcd4
danb11 <miliam5234@naver.com>
2017-08-24 22:19:53 +0900

    애니메이션성공

--- ios/Mukkaebie/Mukkaebie/MenuViewController.swift
+++ ios/Mukkaebie/Mukkaebie/MenuViewController.swift
@@ -36,6 +36,33 @@ class MenuViewController: UIViewController {
         view.frame.size.height = view.frame.size.height - menuTableView.frame.size.height + menuTableView.contentSize.height
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+        self.addCircleView(self.chartAnimationView, isForeground: true, duration: 2, fromValue: 1,  toValue: 0.0)
+
+    }
+    
+    func addCircleView( _ myView : UIView, isForeground : Bool, duration : TimeInterval, fromValue: CGFloat, toValue : CGFloat ) {
+        var circleWidth = CGFloat(90)
+        var circleHeight = circleWidth
+        
+        var circleView = AnimateView(frame: CGRect(x: self.view.bounds.width * 0.5, y: chartAnimationView.bounds.height/2 - 20, width: circleWidth, height: circleHeight))
+        
+        //Setting the color.
+        if (isForeground == true) {
+            circleView.setStrokeColor(UIColor.white.cgColor)
+        }
+        
+        myView.addSubview(circleView)
+        
+        //Rotate the circle so it starts from the top.
+        circleView.transform = CGAffineTransform(rotationAngle: 0)
+        
+        // Animate the drawing of the circle
+        circleView.animateCircleTo(duration, fromValue: fromValue, toValue: toValue)
+    }
+    
     func setSegment() {
         
         pieChartView.segments = []
