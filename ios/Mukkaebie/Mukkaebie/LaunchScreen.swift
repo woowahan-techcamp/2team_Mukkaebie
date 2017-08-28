70c76a835d7640d0bca2837daf042dd0a8d558d4
danb11 <miliam5234@naver.com>
2017-08-28 14:11:00 +0900

    런치스크린추가

---
+++ ios/Mukkaebie/Mukkaebie/LaunchScreen.swift
@@ -0,0 +1,21 @@
+//
+//  LaunchScreen.swift
+//  Mukkaebie
+//
+//  Created by woowabrothers on 2017. 8. 28..
+//  Copyright © 2017년 woowabrothers. All rights reserved.
+//
+
+import UIKit
+
+class LaunchScreen: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
